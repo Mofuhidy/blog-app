d4ba4b0674dcccdb7d127e62e0d68329df04e093
moFuhidy <mo7ammed9290@gmail.com>
2023-09-20 13:15:40 +0300

    Correct the models and add the data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+   first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+   second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+   first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+   second_post = Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
+   third_post = Post.create(author: second_user, title: 'Hello', text: 'This is my third post')
+   fourth_post = Post.create(author: second_user, title: 'Hello', text: 'This is my fourth post')
+
+
+   comment1 = Comment.create(post: first_post, user: first_user, text: 'Hi Tom!' )
+   comment2 = Comment.create(post: second_post, user: first_user, text: 'how are you!' )
+   comment3 = Comment.create(post: third_post, user: first_user, text: 'I like your post!' )
+   comment4 = Comment.create(post: fourth_post, user: second_user, text: 'It is such a nice post!' )
+   comment5 = Comment.create(post: third_post, user: second_user, text: 'I am really inspired!' )
+   comment6 = Comment.create(post: fourth_post, user: second_user, text: 'Bye!' )
