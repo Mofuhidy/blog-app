33f3b0b70a35144a59cc7433852e0ee5aa57ac76
moFuhidy <mo7ammed9290@gmail.com>
2023-09-20 13:31:14 +0300

    Add the custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.increment!(:comment_counter)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,18 +7,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-   first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-   second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                          bio: 'Teacher from Poland.')
 
-   first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-   second_post = Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
-   third_post = Post.create(author: second_user, title: 'Hello', text: 'This is my third post')
-   fourth_post = Post.create(author: second_user, title: 'Hello', text: 'This is my fourth post')
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
+third_post = Post.create(author: second_user, title: 'Hello', text: 'This is my third post')
+fourth_post = Post.create(author: second_user, title: 'Hello', text: 'This is my fourth post')
 
-
-   comment1 = Comment.create(post: first_post, user: first_user, text: 'Hi Tom!' )
-   comment2 = Comment.create(post: second_post, user: first_user, text: 'how are you!' )
-   comment3 = Comment.create(post: third_post, user: first_user, text: 'I like your post!' )
-   comment4 = Comment.create(post: fourth_post, user: second_user, text: 'It is such a nice post!' )
-   comment5 = Comment.create(post: third_post, user: second_user, text: 'I am really inspired!' )
-   comment6 = Comment.create(post: fourth_post, user: second_user, text: 'Bye!' )
+Comment.create(post: first_post, user: first_user, text: 'Hi Tom!')
+Comment.create(post: second_post, user: first_user, text: 'how are you!')
+Comment.create(post: third_post, user: first_user, text: 'I like your post!')
+Comment.create(post: fourth_post, user: second_user, text: 'It is such a nice post!')
+Comment.create(post: third_post, user: second_user, text: 'I am really inspired!')
+Comment.create(post: fourth_post, user: second_user, text: 'Bye!')
