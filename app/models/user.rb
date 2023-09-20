c800ee5d9638504258b91a99648c6e9a65e2497c
moFuhidy <mo7ammed9290@gmail.com>
2023-09-20 11:59:26 +0300

    Add the correct associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :posts, foriegn_key: 'author_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, foriegn_key: 'author_id'
+  has_many :likes, foriegn_key: 'author_id'
 end
