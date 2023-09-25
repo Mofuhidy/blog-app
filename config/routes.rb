5413fecee8e46a91e7990bb21ad69ed7ea071080
moFuhidy <mo7ammed9290@gmail.com>
2023-09-25 13:32:31 +0300

    Fix the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
-
-  get 'users/', to: 'users#index', as: 'users'
-  get 'users/:id', to: 'users/show', as: 'user'
-  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  # root 'users#index'
 end
