157892ada0aa328799defdf9f1153ec9a16d9a83
moFuhidy <mo7ammed9290@gmail.com>
2023-10-04 11:58:07 +0300

    Add the ability roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,16 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
     #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
+    return unless user.present?
+
+    can :manage, Post, author_id: user.id
+    can :destroy, Comment, user_id: user.id
+    can :create, Comment
+    can :read, :all
+
+    return unless user.is? :admin
+    can :manage, :all
+    
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
